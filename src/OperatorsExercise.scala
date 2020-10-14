c536c16183d6ceb1bd6bbd069b07f42ff55bd578
Sameer Sethi <>
2020-10-13 17:27:30 -0700

    Extractors

--- src/OperatorsExercise.scala
+++ src/OperatorsExercise.scala
@@ -13,6 +13,7 @@ object OperatorsExercise extends App{
   }
   println(testExtractor)
 
+  // Email extractor > unapply
   object Email{
     def unapply(email: String): Option[(String, String)] = {
       val atSymbol = email.indexOf("@")
@@ -29,4 +30,70 @@ object OperatorsExercise extends App{
     case _ => Nil
   }
   println(emailExtractor)
+
+  // Uppercase extractor > unapply
+  object Uppercase{
+    def unapply(str: String): Option[String] = Some(str toUpperCase)
+  }
+
+  val city = "los angeles"
+  case class Location(val city: String, val state: String)
+
+  val uppercaseExtractor = Location("los angeles", "ca") match {
+    case Location(Uppercase(city), Uppercase(state)) => s"${city}, ${state}"
+    case _ => Nil
+  }
+  println(uppercaseExtractor)
+
+  // Extract from file path
+  object DestructurePath {
+    def unapply(path: String): Option[Array[String]] = {
+      val arr = path.split("/")
+      if (arr.isEmpty || arr.tail.isEmpty) Some(arr) else Some(arr.tail)
+    }
+    // Alternate approach
+//    def unapply(path: String): Option[Array[String]] = if(path.indexOf("/") == -1) Some(Some(Array(path))).flatten else Some(path.tail.split("/"))
+  }
+
+  // output should be: (level0:Library, level1:Java, level(n):value(n))
+  def toKeyValuePair(pathCollection: Array[String]) = {
+    pathCollection.zipWithIndex
+      .map(a => s"level${a._2} : ${a._1}")
+      .mkString("{", ", ", "}")
+  }
+
+  val samplePath = "/home/cay/readme.txt"
+  val samplePath1 =
+    "/Library/Java/JavaVirtualMachines/jdk1.8.0_221.jdk/Contents/Home/bin/java"
+  val samplePath2 = "test"
+  val samplePath3 = "/"
+
+  val destructurePath: String = samplePath3 match {
+    case ""                    => throw new UnknownError()
+    case "/"                    => s"{Level0: Root}"
+    case DestructurePath(path) => toKeyValuePair(path)
+    case _                     => throw new UnknownError()
+  }
+
+  val destructurePath1 = (path: String) =>
+    if (path == null || path.isEmpty) throw new UnknownError()
+    else
+      path match {
+        case DestructurePath(path) => toKeyValuePair(path)
+        case _                     => throw new UnknownError()
+      }
+
+   println(destructurePath)
+
+  object Positive {
+    def unapply(arg: Int): Option[Int] = if(arg > 0) Some(arg) else None
+  }
+
+  def isPositive(arg: Int): Boolean = {
+    arg match {
+      case Positive(arg) => true
+      case _ => false
+    }
+  }
+
 }
